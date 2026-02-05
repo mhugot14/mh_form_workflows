af2451b64b9c5314248df90d15997f115deb651a
Michael Hugot <hugot@lebk-muenster.de>
2026-02-05 13:48:37 +0100

    Version 1.2.0

--- mh_form_workflows.php
+++ mh_form_workflows.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MH Form Workflows
  * Description: Digitalisierte Formularprozesse mit PDF-Generierung.
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Michael Hugot
  * Text Domain: mh-form-workflows
  * Requires PHP: 8.0
@@ -32,7 +32,7 @@ use Mh\FormWorkflows\Setup\Plugin_Bootstrap;
  */
 define( 'MH_FW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MH_FW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'MH_FW_VERSION', '1.1.0' );
+define( 'MH_FW_VERSION', '1.2.0' );
 
 /**
  * Aktivierungs-Hook: Tabellen erstellen.
